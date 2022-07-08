7f2aa94cc56e11418b6f24f60d62b74bc661000b
Caio Rodrigo de Oliveira <caiooliveira@pmsv.local>
2022-07-08 12:27:12 -0300

    Fomulario criado e estilizado

---
+++ index.js
@@ -0,0 +1,18 @@
+const iniciar =() =>{
+const enviar = document.getElementById('enviar');
+
+const obrigatorio = document.getElementById('obrigatiorio');
+
+// if(obrigatorio = undefined){
+// }
+
+
+
+
+
+// else{
+// enviar.addEventListener('click', () => alert('Inscrição Realizada!')); //leva botão ao link
+// }
+}
+
+document.addEventListener('DOMContentLoaded', iniciar);
